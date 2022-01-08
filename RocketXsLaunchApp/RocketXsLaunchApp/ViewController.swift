7ea5c524ca726656753b14b4b043786c79cbb3a4
Shad Mazumder <shadmazumder@gmail.com>
2022-01-08 17:02:25 +0600

    Initiating RocketXsLaunch App and workspace

---
+++ RocketXsLaunchApp/RocketXsLaunchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RocketXsLaunchApp
+//
+//  Created by Shad Mazumder on 8/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
