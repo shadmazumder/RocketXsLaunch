ae5a5ce9d2199c1fb4c0c9540e0e8bc8567e2fd2
Shad Mazumder <shadmazumder@gmail.com>
2022-01-09 00:53:03 +0600

    Initiating RemoteLoader does not Request for data

--- RocketXsLaunch/RocketXsLaunchTests/RemoteLoaderTests.swift
+++ RocketXsLaunch/RocketXsLaunchTests/RemoteLoaderTests.swift
@@ -7,4 +7,50 @@
 
 import XCTest
 
-class RemoteLoaderTests: XCTestCase {}
+protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+
+    func get(from url: URL, completion: @escaping (Result) -> Void)
+}
+
+final class RemotePostLoader{
+    typealias Result = Swift.Result<[Decodable], Error>
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case non200HTTPResponse
+        case invalidData
+    }
+
+    private let url: URL
+    private let client: HTTPClient
+
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load(completion: @escaping ((Result) -> Void)) {}
+}
+
+class RemoteLoaderTests: XCTestCase {
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+
+        XCTAssertTrue(client.message.isEmpty)
+    }
+    
+    // MARK: - Helper
+    private func makeSUT(_ url: URL = URL(string: "some-url")!) -> (sut: RemotePostLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemotePostLoader(url: url, client: client)
+
+        return(sut, client)
+    }
+}
+
+private class HTTPClientSpy: HTTPClient {
+    var message = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+    
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {}
+}
