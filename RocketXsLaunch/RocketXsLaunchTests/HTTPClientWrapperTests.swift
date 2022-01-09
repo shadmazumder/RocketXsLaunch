72fa4b09e48188ca6a60e069d6cec69688447805
Shad Mazumder <shadmazumder@gmail.com>
2022-01-09 13:39:57 +0600

    Adding stub class to record URLSession activity

--- RocketXsLaunch/RocketXsLaunchTests/HTTPClientWrapperTests.swift
+++ RocketXsLaunch/RocketXsLaunchTests/HTTPClientWrapperTests.swift
@@ -9,3 +9,64 @@ import XCTest
 
 class HTTPClientWrapperTests: XCTestCase {
 }
+
+private class URLProtocolStub: URLProtocol {
+    private struct Stub {
+        let data: Data?
+        let response: URLResponse?
+        let error: Error?
+    }
+
+    private static var stub: Stub?
+    private static var requestObservable: ((URLRequest) -> Void)?
+
+    static func stub(data: Data?, response: URLResponse?, error: Error?) {
+        stub = Stub(data: data, response: response, error: error)
+    }
+
+    static func observeRequest(observer: @escaping ((URLRequest)-> Void)) {
+        requestObservable = observer
+    }
+
+    static func startInterceptingRequest() {
+        URLProtocol.registerClass(URLProtocolStub.self)
+    }
+
+    static func stopInterceptingRequest() {
+        URLProtocol.unregisterClass(URLProtocolStub.self)
+        stub = nil
+        URLProtocolStub.requestObservable = nil
+    }
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override func startLoading() {
+        if let requestObserver = URLProtocolStub.requestObservable {
+            client?.urlProtocolDidFinishLoading(self)
+            return requestObserver(request)
+        }
+
+        if let data = URLProtocolStub.stub?.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+
+        if let response = URLProtocolStub.stub?.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+
+        if let error = URLProtocolStub.stub?.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+
+        client?.urlProtocolDidFinishLoading(self)
+    }
+
+    override func stopLoading() {}
+}
+
