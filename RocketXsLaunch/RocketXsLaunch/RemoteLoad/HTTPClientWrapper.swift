5c024279fcecf7881792062f93b9ea55de72c783
Shad Mazumder <shadmazumder@gmail.com>
2022-01-09 13:50:03 +0600

    Adding HTTPClientWrapper to wrap the Alamofire

---
+++ RocketXsLaunch/RocketXsLaunch/RemoteLoad/HTTPClientWrapper.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPClientWrapper.swift
+//  RocketXsLaunch
+//
+//  Created by Shad Mazumder on 9/1/22.
+//
+
+import Foundation
+
+public class HTTPClientWrapper: HTTPClient {
+    public init() {}
+    
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        
+    }
+}
