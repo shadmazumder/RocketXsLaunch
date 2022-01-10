26eeda3684792709f5e0f99203f541c69946cf98
Shad Mazumder <shadmazumder@gmail.com>
2022-01-10 08:03:32 +0600

    Adding LaunchMapperTests

---
+++ RocketXsLaunch/RocketXsLaunchTests/Launch/LaunchMapperTests.swift
@@ -0,0 +1,14 @@
+//
+//  LaunchMapperTests.swift
+//  RocketXsLaunchTests
+//
+//  Created by Shad Mazumder on 10/1/22.
+//
+
+import XCTest
+
+class LaunchMapperTests: XCTestCase {
+    func test_DecodableLaunch_mapsLaunch() {
+        
+    }
+}
