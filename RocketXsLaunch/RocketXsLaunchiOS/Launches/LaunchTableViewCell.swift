e73a3f8145b8cb8d9242384c635a9062cca67512
Shad Mazumder <shadmazumder@gmail.com>
2022-01-12 00:24:51 +0600

    Adding LaunchTableViewCell

---
+++ RocketXsLaunch/RocketXsLaunchiOS/Launches/LaunchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LaunchTableViewCell.swift
+//  RocketXsLaunchiOS
+//
+//  Created by Shad Mazumder on 12/1/22.
+//
+
+import UIKit
+
+class LaunchTableViewCell: UITableViewCell {
+    static let ReuseableIdentifier = "LaunchTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
