4e6dbcc8dda09c8d2bdb6eab1d9b079884ec2403
Shad Mazumder <shadmazumder@gmail.com>
2022-01-11 22:07:06 +0600

    Loading LaunchViewController from storyboard

---
+++ RocketXsLaunch/RocketXsLaunchiOS/Launches/LaunchesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LaunchesViewController.swift
+//  RocketXsLaunchiOS
+//
+//  Created by Shad Mazumder on 11/1/22.
+//
+
+import UIKit
+import RocketXsLaunch
+
+public final class LaunchesViewController: UIViewController {
+    public var remoteLoader: RemoteLoader<LaunchAPIModel>?
+}
--- RocketXsLaunch/RocketXsLaunchiOSTests/RocketXsLaunchiOSTests.swift
+++ RocketXsLaunch/RocketXsLaunchiOSTests/RocketXsLaunchiOSTests.swift
@@ -6,9 +6,49 @@
 //
 
 import XCTest
+import RocketXsLaunch
+import RocketXsLaunchiOS
 
 class RocketXsLaunchiOSTests: XCTestCase {
-    func test() {
-        XCTAssertTrue(true)
+    func test_init_doesNotStartLoading() {
+        var counter = 0
+        struct Client: HTTPClient{
+            func get(from url: URL, completion: @escaping (HTTPResult) -> Void) {}
+        }
+        
+        let loader = RemoteLoader<LaunchAPIModel>(url: URL(string: "any-url")!, client: Client())
+        loader.load { _ in
+            counter = +1
+        }
+        
+        let _ = makeSUT(loader: loader)
+        XCTAssertEqual(counter, 0)
+    }
+    
+    // MARK: - Helper
+    private func makeSUT(_ headerText: String = "", loader: RemoteLoader<LaunchAPIModel>) -> LaunchesViewController {
+        let sut = LaunchesViewControllerFromPostSotyboard()
+        sut.remoteLoader = loader
+        sut.title = headerText
+        
+        trackMemoryLeak(sut)
+        trackMemoryLeak(loader)
+        
+        return sut
+    }
+    
+    private func LaunchesViewControllerFromPostSotyboard() -> LaunchesViewController {
+        let bundle = Bundle(for: LaunchesViewController.self)
+        let storyboard = UIStoryboard(name: "Launches", bundle: bundle)
+        return storyboard.instantiateInitialViewController() as! LaunchesViewController
     }
 }
+
+extension XCTestCase {
+    func trackMemoryLeak(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Memory Leak!!! Didn't deallocated", file: file, line: line)
+        }
+    }
+}
+
