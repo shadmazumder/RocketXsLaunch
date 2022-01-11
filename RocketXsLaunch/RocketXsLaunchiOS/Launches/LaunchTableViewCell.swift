32a4121a300a12784d9b88a041a31df4109e3083
Shad Mazumder <shadmazumder@gmail.com>
2022-01-12 00:28:58 +0600

    Connecting Cell outlet

--- RocketXsLaunch/RocketXsLaunchiOS/Launches/LaunchTableViewCell.swift
+++ RocketXsLaunch/RocketXsLaunchiOS/Launches/LaunchTableViewCell.swift
@@ -10,15 +10,9 @@ import UIKit
 class LaunchTableViewCell: UITableViewCell {
     static let ReuseableIdentifier = "LaunchTableViewCell"
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var details: NSLayoutConstraint!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var launchNumber: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var patchImageView: UIImageView!
 }
